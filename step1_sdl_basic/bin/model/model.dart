0b291e754f3744c3915a1b6f01609b8d6ce2fb5a
wdevore <william.quartz@gmail.com>
2024-03-30 23:48:49 -0400

    step1 up to ortho projection.

---
+++ step1_sdl_basic/bin/model/model.dart
@@ -0,0 +1,44 @@
+import 'package:vector_math/vector_math.dart';
+
+class Model {
+  final List<Vector3> points = [];
+  final List<Vector3> projPoints = [];
+
+  final double fov_factor = 128.0;
+
+  void update() {
+    int i = 0;
+    for (Vector3 p in points) {
+      // Project point
+      Vector3 pp = orthoProject(p);
+      projPoints[i] = pp;
+      i++;
+    }
+  }
+
+  void build() {
+    // Form a Cube 2 x 2 x 2
+    for (var x = -1.0; x <= 1; x += 0.25) {
+      for (var y = -1.0; y <= 1; y += 0.25) {
+        for (var z = -1.0; z <= 1; z += 0.25) {
+          points.add(Vector3(x, y, z));
+        }
+      }
+    }
+    // For now just copy
+    projPoints.addAll(points);
+  }
+
+  /// Orthographic projection
+  Vector3 orthoProject(Vector3 point) {
+    Vector3 v = Vector3.zero()
+      ..setValues(fov_factor * point.x, fov_factor * point.y, point.z);
+    return v;
+  }
+
+  /// Perspective projection
+  Vector3 perspProject(Vector3 point) {
+    Vector3 v = Vector3.zero();
+    return v;
+  }
+}
